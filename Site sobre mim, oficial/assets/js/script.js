cebe1876353053120ca7d6bc8e65c394779aabd2
Daniel Campos <106026946+danzcampos@users.noreply.github.com>
2024-03-03 21:03:55 -0300

    Delete Site sobre mim, oficial directory

--- Site sobre mim, oficial/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const toggleButton = document.getElementById("toggle-button");
-  const pmain = document.querySelector(".Pmain");
-
-  toggleButton.addEventListener("click", () => {
-    toggleButton.classList.toggle("expanded");
-    pmain.classList.toggle("expanded");
-  });
-});
